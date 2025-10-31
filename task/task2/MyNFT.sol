829303e37d3b441ea2bb63056760069511697832
SolitaryZJ <solitary_J@163.com>
2025-10-31 19:04:37 +0800

    optimize the MyNFT

--- task/task2/MyNFT.sol
+++ task/task2/MyNFT.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract MyNFT is ERC721URIStorage {
+
+contract MyNFT is ERC721URIStorage , Ownable{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor(address initialOwner) ERC721("MyNFT", "MNFT") Ownable(initialOwner){}
 
     function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
